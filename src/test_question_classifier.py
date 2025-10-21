46ce556d91ba6bd3db941b20f5aaf85757ceab23
GabrielaFort <gabriela.fort@hci.utah.edu>
2025-10-21 12:57:59 -0600

    add tests for stats

--- src/test_question_classifier.py
+++ src/test_question_classifier.py
@@ -1,14 +1,19 @@
 from question_classifier import QuestionClassifier
 from llm_client import LLMClient
-from utils import llama3_llm   
+from utils import stats_llm   
 
 # Instantiate question classifier
-classifier = QuestionClassifier(llama3_llm())
+classifier = QuestionClassifier(stats_llm())
 
 questions = [
     "How many patients had colitis?",
     "Plot number of patients per tumor type.",
-    "Females"
+    "Females",
+    "Show me a histogram of age distribution.",
+    "What is the average survival time?",
+    "Is there a significant difference in time to onset between males and females?",
+    "Is there a difference in irae types between age groups?"
+
 ]
 
 for q in questions:

8603ed9ad7427adf783e775c38e07ae035368535
GabrielaFort <gabriela.fort@hci.utah.edu>
2025-12-02 08:54:29 -0700

    add guidelie agent

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -2,5 +2,6 @@ from .query_agent import QueryAgent
 from .plot_agent import PlotAgent
 from .error_agent import ErrorAgent
 from .stats_agent import StatsAgent
+from .guideline_agent import GuidelineAgent
 
 

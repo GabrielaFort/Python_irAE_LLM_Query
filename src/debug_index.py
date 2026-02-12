76ffb4e0dc3a332b4fd8674ccb766a50da178ba5
GabrielaFort <gabriela.fort@hci.utah.edu>
2026-02-12 10:38:34 -0700

    remove old files

--- src/debug_index.py
+++
@@ -1,8 +0,0 @@
-# debug_index.py
-from index_manager import IndexManager
-im = IndexManager()
-im.load()
-print("Loaded", len(im.metadatas), "chunks.")
-results = im.search("first-line therapy for metastatic melanoma", top_k=3)
-for r in results:
-    print(r['doc_key'], r['section_title'][:80], r['score'])
